2020b4c13323334dcc4b95be4da4aa8dcff831cf
happy_panda <2664311541@qq.com>
2023-12-07 14:08:49 +0800

    提交测试

--- kernel/test1.cpp
+++ kernel/test1.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
     cout << "hello world!";
+    vector<int> v({1, 2, 3, 4});
+    for (auto i : v)
+        cout << i;
     return 0;
 }
